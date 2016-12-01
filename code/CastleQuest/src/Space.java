f41761b191d846e5453c6a3c11d0fcc347e38881
sarah-mangels <sarahmangels@Sarahs-MacBook-Pro.local>
2016-11-30 19:02:23 -0500

    player and space empty classes

---
+++ code/CastleQuest/src/Space.java
@@ -0,0 +1,39 @@
+/* Space class
+ * spaces have types and pointers to neighbors
+ * Spaces can also house traps or keys
+ * keys are player dependent, so only the player who needs this key could get it
+ */
+
+public class Space implements SpaceInterface {
+
+	@Override
+	public Space[] getNeighbors() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setNeighbors() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPlayers() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isActive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setActive(boolean a) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
