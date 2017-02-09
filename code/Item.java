6843d72e6001bf4148a06469cc4f9fd775a2a14e
dlassalle <dlassalle@umass.edu>
2017-02-09 11:16:44 -0500

    removed extra files

--- code/Item.java
+++
@@ -1,9 +0,0 @@
-/* class for item types
- * items can be instantiated from here, each type has its own effects
- * Potions
- * magical items
- * traps
- * keys of greatness
- * weapons
- * armor
- */
--- code/Player.java
+++
@@ -1,8 +0,0 @@
-/*Contains the inventory of the player as well as the player stats
- * will house functions for checking inventory, adding/removing items from inventory
- * adding/removing from inventory should refresh HP/CP
- * pointer to next player in order
- * adding/removing health in combat
- * where the player is
- * LED color, player looks, other customizations
- */
